ae1f2ef480095c9d7e0064ae622d3039c51a0b01
Carina <carinajh99@gmail.com>
2023-08-02 08:47:49 -0700

    more changes

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,31 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Welcome to this island.";
+let name = "Aqua";
+let title = "God of the Oceans";
+let worldName = "Olympus";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 1000;
+let year = 900;
+let century = 100;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weapons = ['Trident','Sword','Bow and Arrow', 'Daggers']
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let hasFavWeapon = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + "My name is " + name + ".");
+console.log("I'm referred to as " + title);
+console.log("I live in " + worldName + ".");
+console.log("In this world I am " + age + " years old.");
+console.log("It is currently the year " + year + " and the century " + century + ".");
+console.log("I use multiple different weapons such as " + weapons[0] + ", " + weapons[1] + ", " + weapons[2] + ", and " + weapons[3] + ".");
+console.log( + hasFavWeapon + "my favorite weapon has always been " + weapons[0] + ".");
+console.log("I hope you enjoy your stay here!");
